966ee479bba8ccddb4c4ceb7f99c823027ba263f
jacedoir <remi.grz@free.fr>
2023-10-15 17:16:30 +0200

    Creating the map

---
+++ Map/affichage.php
@@ -0,0 +1,19 @@
+<?php
+include 'config.php';
+
+$mysqli = new mysqli($host, $user, $password, $database);
+$mysqli->set_charset("utf8");
+
+if ($mysqli->connect_error) {
+    die('Erreur de connexion à la base de données : ' . $mysqli->connect_error);
+}
+
+$result = $mysqli->query("SELECT * FROM Cities");
+$data = [];
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+echo json_encode($data);
+
+$mysqli->close();
+?>
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # INF552_Projet
+Installation d'un server PHP pour le backend
+Utiliser l'extension Live Server (Five Server)
+Suivre la configuration de PHP
+
+Penser à mettre ses identifiants DB dans config.php
