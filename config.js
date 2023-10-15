e268e6cc0f60b6a88826e2debe98e82b8c17076e
jacedoir <remi.grz@free.fr>
2023-10-15 16:08:37 +0200

    Create config.js
    Fichier template pour les identifiants

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: {
+      host: 'localhost',
+      user: 'votre_utilisateur',
+      password: 'votre_mot_de_passe',
+      database: 'votre_base_de_donnees',
+    },
+  };
