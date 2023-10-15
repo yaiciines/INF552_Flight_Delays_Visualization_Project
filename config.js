5976ba45f11eeaf6d64e960bae5c643bf3fb4be8
jacedoir <remi.grz@free.fr>
2023-10-15 17:19:06 +0200

    Add config file

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    db: {
-      host: 'localhost',
-      user: 'votre_utilisateur',
-      password: 'votre_mot_de_passe',
-      database: 'votre_base_de_donnees',
-    },
-  };
