4850531b4caf9ac7363410cb0e2bc576f7176f5c
jacedoir <remi.grz@free.fr>
2023-10-19 18:12:13 +0200

    Using d3

--- Map/config.php
+++ Map/config.php
@@ -1,6 +1,6 @@
 <?php
 $host="gr3z.eu";
-$user="";//ton user
-$password=""; //ton password
+$user="x9p31ij2_inf552";
+$password="Y7!8F&HV*vb^V*kep^op";
 $database="x9p31ij2_inf552";
 ?>
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Utiliser l'extension Live Server (Five Server)
 Suivre la configuration de PHP
 
 Penser à mettre ses identifiants DB dans config.php
+![Alt text](image.png)
