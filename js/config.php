131a243df9412ffe281b191c7c53e17a56d10f4b
yaiciines <ines.yaici@polytechnique.edu>
2023-10-20 17:58:15 +0200

    adding php files

---
+++ js/config.php
@@ -0,0 +1,6 @@
+<?php
+$host="gr3z.eu";
+$user="x9p31ij2_ines";
+$password="LNd&HobOGQN_";
+$database="x9p31ij2_inf552";
+?>
--- js/test_visualizations.js
+++ js/test_visualizations.js
@@ -3,6 +3,102 @@ const ctx = {
     h: 720,
     JITTER_W:50
 };
+// Assuming you have D3 and D3-fetch libraries loaded
+// Replace "your_data.csv" with the path to your data file
+function processDelayData(data) {
+    const airlines = data.map(d => d.AIRLINE);
+
+    const delayColumns = ['AIR_SYSTEM_DELAY', 'AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY', 'SECURITY_DELAY', 'WEATHER_DELAY'];
+
+    const countByAirline = {};
+    const countWithDelays = {};
+
+    airlines.forEach(airline => {
+        countByAirline[airline] = {};
+        countWithDelays[airline] = 0;
+    });
+
+    data.forEach(d => {
+        delayColumns.forEach(delayColumn => {
+            if (parseInt(d[delayColumn]) > 0) {
+                countWithDelays[d.AIRLINE]++;
+                countByAirline[d.AIRLINE][delayColumn] = (countByAirline[d.AIRLINE][delayColumn] || 0) + 1;
+            }
+        });
+    });
+
+    console.log(countByAirline);
+
+    const percentages = airlines.map(airline => {
+        const totalDelays = countWithDelays[airline];
+        return {
+            AIRLINE: airline,
+            ...delayColumns.reduce((result, column) => {
+                result[column] = (countByAirline[airline][column] || 0) / totalDelays * 100;
+                return result;
+            }, {})
+        };
+    });
+
+    console.log(percentages);
+
+
+    // Now, you can create a bar chart using D3.js or any other library of your choice.
+    // For D3.js, you can do something like this:
+
+    const width = 600;
+    const height = 400;
+    const margin = { top: 30, right: 30, bottom: 60, left: 60 };
+
+    const svg = d3.select("#rootG")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    const x = d3.scaleBand()
+        .domain(airlines)
+        .range([margin.left, width - margin.right])
+        .padding(0.1);
+
+    const y = d3.scaleLinear()
+        .domain([0, 100]) // Assuming percentages range from 0 to 100
+        .nice()
+        .range([height - margin.bottom, margin.top]);
+
+    const color = d3.scaleOrdinal()
+        .domain(delayColumns)
+        .range(['#e45755', '#67a9cf', '#f46d43', '#d73027', '#74add1']);
+
+    const xAxis = g => g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("transform", "rotate(-45)");
+
+    const yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y));
+
+    svg.append("g")
+        .selectAll("g")
+        .data(delayColumns)
+        .enter().append("g")
+        .attr("fill", color)
+        .selectAll("rect")
+        .data(d => percentages.map(p => ({ AIRLINE: p.AIRLINE, delay: d, percentage: p[d] })))
+        .enter().append("rect")
+        .attr("x", d => x(d.AIRLINE) + x.bandwidth() / delayColumns.length * delayColumns.indexOf(d.delay))
+        .attr("y", d => y(d.percentage))
+        .attr("width", x.bandwidth() / delayColumns.length)
+        .attr("height", d => y(0) - y(d.percentage));
+
+    svg.append("g").call(xAxis);
+
+    svg.append("g").call(yAxis);
+};
+
+/* ==================================================================================*/
 
 function loadData() {
     Promise.all([
@@ -18,12 +114,14 @@ function loadData() {
         // Now you can work with each dataset separately.
         let flightjanuary = flightsData.filter((d) => d.MONTH == 1);
         console.log(`Number of flights in January: ${flightjanuary.length}`);
-        
+
+        processDelayData(flightsData);
         // You can also access airport and third data in a similar way.
     }).catch(function (error) {
         console.log(error);
     });
 }
+
 
 
 /*function loadData() {
